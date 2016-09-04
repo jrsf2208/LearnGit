df6731cb56623d1d00cdce3f83d736543663aeb4
jrsf2208 <jrsf2208@gmail.com>
2016-09-04 14:27:14 -0500

    Change 1

--- LearnGit/__init__.py
+++ LearnGit/__init__.py
@@ -1,39 +1,39 @@
-class Student(object):
-    def __init__(self, name="", school="", grade=""):
+class Driver(object):
+    def __init__(self, name="", track="", finish=""):
         if not name:
-            name = input("What is the student's name? ")
-        if not school:
-            school = input("What is the student's school? ")
-        if not grade:
-            grade = self.get_grade()
+            name = input("What is the driver's name? ")
+        if not track:
+            track = input("What is the driver's track? ")
+        if not finish:
+            finish = self.get_finish()
         self.name = name
-        self.school = school
-        self.grade = grade
-    def get_grade(self):
+        self.track = track
+        self.finish = finish
+    def get_finish(self):
         while True:
-            grade = input("What is the student's grade ? [K, 1-5]  ")
-            if grade.lower() not in ['k', '1', '2', '3', '4', '5']:
-                print("I'm sorry, but {} isn't valide.".format(grade))
+            finish = input("What is the driver's finish ? [K, 1-5]  ")
+            if finish.lower() not in ['k', '1', '2', '3', '4', '5']:
+                print("I'm sorry, but {} isn't valid.".format(finish))
             else:
-                return grade
+                return finish
 
-    def print_student(self):
+    def print_driver(self):
         print("Name: {}".format(self.name))
-        print("School {}".format(self.school))
-        print("Grade {}".format(self.grade))
+        print("Track {}".format(self.track))
+        print("Finish {}".format(self.finish))
 
-def print_roster(students):
-    print("Students in the system:")
-    for student in students:
+def print_roster(drivers):
+    print("Drivers in the system:")
+    for driver in drivers:
         print("*"*15)
-        student.print_student()
+        driver.print_driver()
 
 def main():
-    student1 = Student(name="Carrie Kale", grade="3", school="Marshall")
-    student2 = Student(name="Byron Bale", grade="2", school="Minnieville")
-    student3 = Student(name="Sarah Chandler", grade="K", school="Woodbridge")
-    students = [student1, student2, student3]
-    print_roster(students)
+    driver1 = Driver(name="Car 18", finish="3", track="Marshall")
+    driver2 = Driver(name="Car 19", finish="2", track="Minnieville")
+    driver3 = Driver(name="Car 20", finish="K", track="Woodbridge")
+    drivers = [driver1, driver2, driver3]
+    print_roster(drivers)
 
 if __name__ == '__main__':
     main()
